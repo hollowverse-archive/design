8510d172c3b9ce6efc4b63860ec34117cc1afd66
Maxgrb <maksand@me.com>
2017-08-13 16:19:18 +0200

    Update namming according to /typings/dataSchema.d.ts

--- src/components/PersonDetails/index.js
+++ src/components/PersonDetails/index.js
@@ -4,10 +4,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as strings from '../../constants/UIStrings';
-import Tag from '../Tag';
+import Label from '../Label';
 import './styles.css';
 
-const renderTag = (i, index) => <Tag key={index} label={i} />;
+const renderLabel = (i, index) => <Label key={index} text={i} />;
 
 const renderParagraph = (i, index) => <p key={index}>{i}</p>;
 
@@ -15,7 +15,7 @@ const PersonDetails = props => (
   <div className="person-details">
     <div
       className="person-details-avatar"
-      style={{ backgroundImage: `url(assets/${props.avatar})` }}
+      style={{ backgroundImage: `url(${props.photoUrl})` }}
     />
     <div className="person-details-caption">
       {strings.RELIGION_POLITICS_AND_IDEAS_OF}
@@ -23,8 +23,8 @@ const PersonDetails = props => (
     <div className="person-details-name">
       {props.name}
     </div>
-    <div className="person-details-tags">
-      {props.tags.map(renderTag)}
+    <div className="person-details-labels">
+      {props.labels.map(renderLabel)}
     </div>
     <div className="person-details-about">
       {props.about.map(renderParagraph)}
@@ -34,8 +34,8 @@ const PersonDetails = props => (
 
 PersonDetails.propTypes = {
   name: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  tags: PropTypes.arrayOf(PropTypes.string),
+  photoUrl: PropTypes.string.isRequired,
+  labels: PropTypes.arrayOf(PropTypes.string),
   about: PropTypes.arrayOf(PropTypes.string),
 };
 
