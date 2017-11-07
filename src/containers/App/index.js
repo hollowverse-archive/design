a133ad493d397645b04baf3f17f213751b3d140a
Maxgrb <maksand@me.com>
2017-11-07 18:10:32 +0100

    Little fix of layout and fix of moment of Facebook SDK init

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -23,17 +23,6 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    // Init Facabook SDK
-    /* eslint-disable */
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    /* eslint-enable */
-
     // Emulate loading and error
     setTimeout(this.handleLoaded, (Math.random() * 1000) + 100);
   }
@@ -42,8 +31,20 @@ export default class App extends Component {
     this.setState({
       isLoading: false,
       errorMessage: getRandomError(),
-    });
+    }, this.initFacebookSdk);
   }
+
+  /* eslint-disable */
+  initFacebookSdk() {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+  /* eslint-enable */
 
   render() {
     return ([
