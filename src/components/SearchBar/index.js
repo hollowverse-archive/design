4dabaf79eb1214a4d07c38236f1c7e6ce61569f8
Maxgrb <maksand@me.com>
2017-12-12 16:48:28 +0100

    Add search dropdown #12

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,30 +1,71 @@
 /**
  * SearchBar Component
  */
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { paths } from '../../constants';
+import { Loader } from '../../components';
 import './styles.css';
 
-const SearchBar = ({
-  anim, margins, searchValue, autoFocus, onChange, onKeyDown,
-}) => {
-  const handleSearchFocus = e => e.target.select();
+export default class SearchBar extends Component {
+  state = {
+    isLoading: false,
+    open: false,
+  }
 
-  return (
-    <div className={classNames('search-bar', { anim, margins })}>
-      <input
-        type="text"
-        className="navbar-search"
-        value={searchValue}
-        autoFocus={autoFocus}
-        placeholder="Search..."
-        onFocus={handleSearchFocus}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        maxLength={50}
-      />
-    </div>
-  );
-};
+  componentWillReceiveProps(newProps) {
+    const { searchValue } = this.props;
 
-export default SearchBar;
+    if (!searchValue && newProps.searchValue) {
+      this.setState({ isLoading: true, open: false });
+      this.loadingTimeout = setTimeout(this.handleLoaded, (Math.random() * 1000) + 500);
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.loadingTimeout);
+  }
+
+  get searchBody() {
+    const { searchValue } = this.props;
+
+    return (
+      <div className="search-bar-body">
+        <Link to={paths.SEARCH}>
+          {`See results for ${searchValue}`}
+        </Link>
+      </div>
+    );
+  }
+
+  handleLoaded = () => this.setState({ isLoading: false, open: true });
+
+  handleSearchFocus = e => e.target.select();
+
+  render() {
+    const {
+      anim, margins, searchValue, autoFocus, onChange, onKeyDown,
+    } = this.props;
+
+    const { isLoading, open } = this.state;
+
+    return (
+      <div className={classNames('search-bar', { anim, margins, loading: isLoading })}>
+        <input
+          type="text"
+          className="search-bar-input"
+          value={searchValue}
+          autoFocus={autoFocus}
+          placeholder="Search..."
+          onFocus={this.handleSearchFocus}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          maxLength={50}
+        />
+        {isLoading && <Loader small />}
+        {open > 0 && this.searchBody}
+      </div>
+    );
+  }
+}
