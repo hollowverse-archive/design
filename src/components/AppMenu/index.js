c8fc67a2f0f936914a77ceae8b04b77875fcce54
Max Gorbachev <maksand@me.com>
2018-05-18 15:02:04 +0200

    Fix closing with X button #25

--- src/components/AppMenu/index.js
+++ src/components/AppMenu/index.js
@@ -111,6 +111,7 @@ export default class AppMenu extends Component {
             title="Close menu"
             aria-label="Close menu"
             className="app-menu-close"
+            onClick={this.handleClose}
           />
           {this.user}
           <Link
